d00eee58f7991a39d235ccc1fe271d4d118f9bb9
unifjgarcia <149002145+unifjgarcia@users.noreply.github.com>
2023-11-10 18:17:46 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include "lab.h"
+
+int main() {
+    // irformando nossas variaveis e a opção de erro do arquivo.
+    FILE *arquivo;
+    arquivo = fopen("tarefas.dat", "ab+");
+    if (arquivo == NULL) {
+        perror("Erro ao abrir arquivo de tarefas");
+        return 1;
+    }
+
+    int opcao;
+// criação da parte do Menu mais estética
+    do {
+        printf("Menu:\n");
+        printf("1. Cadastrar Tarefa\n");
+        printf("2. Listar Tarefas\n");
+        printf("3. Deletar Tarefa\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+//Logica do Menu
+        switch (opcao) {
+            case 1:
+                cadastrarTarefa(arquivo);
+                break;
+            case 2:
+                listarTarefas(arquivo);
+                break;
+            case 3:
+                deletarTarefa(arquivo);
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    } while (opcao != 4);
+
+    fclose(arquivo);
+
+    return 0;
+}
 
