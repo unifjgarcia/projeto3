ca6a577ae542d4301b7f8b6a8abcfbcb7ba5a325
unifjgarcia <149002145+unifjgarcia@users.noreply.github.com>
2023-11-10 22:21:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 #include "lab.h"
 
 int main() {
+    // Abre o arquivo de tarefas no modo de leitura e escrita binária
     FILE *arquivo = fopen("tarefas.dat", "ab+");
     if (arquivo == NULL) {
         perror("Erro ao abrir o arquivo de tarefas");
         return 1;
     }
-
+    // Variável para armazenar a opção do usuário no menu
     int opcao;
-
+    // Loop do menu principal
     do {
         printf("Menu:\n");
         printf("1. Cadastrar Tarefa\n");
@@ -17,8 +18,11 @@ int main() {
         printf("3. Deletar Tarefa\n");
         printf("4. Sair\n");
         printf("Escolha uma opcao: ");
+        
+        // Recebe a opção escolhida pelo usuário
         scanf("%d", &opcao);
 
+        // Executa a operação correspondente à opção escolhida
         switch (opcao) {
             case 1:
                 cadastrarTarefa(arquivo);
@@ -36,6 +40,6 @@ int main() {
                 printf("Opcao invalida. Tente novamente.\n");
         }
     } while (opcao != 4);
-
+    // Retorna 0 indicando que o programa foi executado com sucesso
     return 0;
 }
